5df2e2165d009aa72dbdc879b6d193106d939ab2
Saul Urrea <saul5577@gmail.com>
2024-06-01 10:28:06 -0500

    initial commit

---
+++ app/src/main/java/com/saulodev/melichallenge/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.saulodev.melichallenge.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Yellow80 = Color(0xFFFBC02D)
+val YellowGray80 = Color(0xFFECEFF1)
+val Gray80 = Color(0xFFB0BEC5)
+
+val Yellow40 = Color(0xFFFBC02D)
+val YellowGray40 = Color(0xFFCFD8DC)
+val Gray40 = Color(0xFF78909C)
---
+++ app/src/main/java/com/saulodev/melichallenge/ui/theme/Theme.kt
@@ -0,0 +1,51 @@
+package com.saulodev.melichallenge.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val LightColorScheme = lightColorScheme(
+    primary = Yellow40,
+    secondary = YellowGray40,
+    tertiary = Gray40,
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onTertiary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black
+)
+
+@Composable
+fun MeliChallengeTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = LightColorScheme
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
